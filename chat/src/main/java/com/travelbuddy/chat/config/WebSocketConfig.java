815e8ed1037020d70de5ec1eb63ef06a64e76c9d
Avishekh1 <avishekh.thakur@zemosolabs.com>
2024-08-14 22:47:54 +0530

    exposed an api for deleting chat record

--- chat/src/main/java/com/travelbuddy/chat/config/WebSocketConfig.java
+++ chat/src/main/java/com/travelbuddy/chat/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://astonishing-kitsune-6007fe.netlify.app").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("https://astonishing-kitsune-6007fe.netlify.app","http://localhost:5173").withSockJS();
     }
 }
