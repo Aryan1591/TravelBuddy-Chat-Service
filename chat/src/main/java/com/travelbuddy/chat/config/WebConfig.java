81ad33bbae6a1d4faf9f71fefe87b5a46b538807
Yagnasree D <88225006+Ysree22@users.noreply.github.com>
2024-08-15 01:19:30 +0530

    Update WebConfig.java

--- chat/src/main/java/com/travelbuddy/chat/config/WebConfig.java
+++ chat/src/main/java/com/travelbuddy/chat/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/")
-                .allowedOrigins("https://coruscating-gaufre-9c3549.netlify.app","https://travelbuddy-posts-service-production.up.railway.app" ,"http://localhost:8080","http://localhost:5173")
+                .allowedOrigins("https://66bd09f19028ae026b5372d9--courageous-bublanina-dd5958.netlify.app","https://travelbuddy-posts-service-production.up.railway.app" ,"http://localhost:8080","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
