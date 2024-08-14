94ce6b654f33d404bf83cb679363c45d3b0d2c6b
Avishekh1 <avishekh.thakur@zemosolabs.com>
2024-08-14 22:47:54 +0530

    exposed an api for deleting chat record

--- chat/src/main/java/com/travelbuddy/chat/service/MongoService.java
+++ chat/src/main/java/com/travelbuddy/chat/service/MongoService.java
@@ -1,8 +1,6 @@
 package com.travelbuddy.chat.service;
 
 import com.travelbuddy.chat.entity.ChatRoom;
-import com.travelbuddy.chat.entity.OnlineUsers;
-import com.travelbuddy.chat.exception.RoomNotFoundException;
 import com.travelbuddy.chat.model.Message;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -10,10 +8,8 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 @Service
 public class MongoService {
@@ -49,43 +45,9 @@ public class MongoService {
         return Objects.isNull(chatRoom) ? List.of() : chatRoom.getMessageList();
     }
 
-    public OnlineUsers saveOnlineUsersRoom(OnlineUsers onlineUsersSpace) {
-        onlineUsersSpace.setUsernameList(new HashSet<>());
-        return mongoTemplate.insert(onlineUsersSpace);
-    }
-
-    public Set<String> getAllOnlineUsersForRoom(String roomId) {
+    public void deleteChatRecord(String chatRoomId) {
         Query query = new Query();
-        query.addCriteria(Criteria.where("id").is(roomId));
-        OnlineUsers onlineUsersSpace = mongoTemplate.findOne(query, OnlineUsers.class);
-        if (Objects.isNull(onlineUsersSpace))
-            throw new RoomNotFoundException("This Room doesn't exist");
-        return onlineUsersSpace.getUsernameList();
+        query.addCriteria(Criteria.where("roomId").is(chatRoomId));
+        mongoTemplate.remove(query, "travelbuddy.chats");
     }
-
-    public void addUserToRoom(String roomId, String userName) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("id").is(roomId));
-        OnlineUsers onlineUsersSpace = mongoTemplate.findOne(query, OnlineUsers.class);
-        if (Objects.isNull(onlineUsersSpace))
-            throw new RoomNotFoundException("This Room doesn't exist");
-        onlineUsersSpace.getUsernameList().add(userName);
-        Update update = new Update();
-        update.set("usernameList", onlineUsersSpace.getUsernameList());
-        mongoTemplate.updateFirst(query, update, OnlineUsers.class);
-    }
-
-    public void removeUserFromRoom(String roomId, String userName) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("id").is(roomId));
-        OnlineUsers onlineUsersSpace = mongoTemplate.findOne(query, OnlineUsers.class);
-        if (Objects.isNull(onlineUsersSpace))
-            throw new RoomNotFoundException("This Room doesn't exist");
-        onlineUsersSpace.getUsernameList().remove(userName);
-        Update update = new Update();
-        update.set("usernameList", onlineUsersSpace.getUsernameList());
-        mongoTemplate.updateFirst(query, update, OnlineUsers.class);
-    }
-
-
 }
